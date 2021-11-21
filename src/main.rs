7745df71563802c6b8faf2af06af629514af5f1b
thekuwayama <thekuwayama@gmail.com>
2021-11-21 18:47:35 +0900

    add print_loop

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,19 @@ use std::time::Duration;
 
 use console::Term;
 
-fn main() {
-    let ss = [" (」・ω・)」うー！", " (／・ω・)／にゃー！"];
+fn print_loop(ss: &[&str], interval: u64) -> ! {
     let term = Term::stdout();
     loop {
         ss.iter().for_each(|s| {
             term.write_line(s).unwrap();
-            thread::sleep(Duration::from_millis(1500));
+            thread::sleep(Duration::from_millis(interval));
             term.move_cursor_up(1).unwrap();
             term.clear_line().unwrap();
         });
     }
+}
+
+fn main() {
+    let ss = [" (」・ω・)」うー！", " (／・ω・)／にゃー！"];
+    print_loop(&ss, 1500)
 }

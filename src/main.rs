bd676135170fa3ea9b521124709df6d137e96e7e
thekuwayama <thekuwayama@gmail.com>
2022-08-29 12:15:19 +0900

    feat: build_loop_s

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use console::Term;
+use unicode_width::UnicodeWidthStr;
 
 fn print_loop(ss: &[&str], interval: u64) -> ! {
     let term = Term::stdout();
+    let now = Instant::now();
     loop {
         ss.iter().for_each(|s| {
-            term.write_line(s).unwrap();
+            let (_, x) = term.size();
+            let s = build_loop_s(&s, now.elapsed().as_secs() as usize, x as usize);
+            term.write_line(&s).unwrap();
             thread::sleep(Duration::from_millis(interval));
             term.move_cursor_up(1).unwrap();
             term.clear_line().unwrap();
@@ -15,7 +19,26 @@ fn print_loop(ss: &[&str], interval: u64) -> ! {
     }
 }
 
+fn build_loop_s(s: &str, head: usize, max: usize) -> String {
+    let s = " ".repeat(head % max) + s;
+    let (s, t) = unicode_trancate(&s, max);
+    let t_width = t.width();
+    t + &s[t_width..]
+}
+
+fn unicode_trancate(s: &str, max: usize) -> (String, String) {
+    let mut s = s.clone().to_string();
+    let mut buf = "".to_string();
+    while s.width() > max {
+        if let Some(t) = s.pop() {
+            buf = String::from(t) + &buf;
+        }
+    }
+
+    (s, buf)
+}
+
 fn main() {
     let ss = [" (」・ω・)」うー！", " (／・ω・)／にゃー！"];
-    print_loop(&ss, 1500)
+    print_loop(&ss, 1000)
 }
